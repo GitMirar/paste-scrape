b0e5189da08de910d2cebcc13b78ef2c0ab8b907
Sascha Steinbiss <satta@debian.org>
2017-08-24 23:33:34 +0200

    add support for daily indices

--- elasticstorage.go
+++ elasticstorage.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"time"
 
 	elastic "gopkg.in/olivere/elastic.v5"
 )
 
-const mapping = `
-{
+const mapping = `{
    "settings":{
       "number_of_shards":1,
       "number_of_replicas":0
@@ -54,7 +55,29 @@ type ElasticStorageModule struct {
 	index        string
 	elasticURL   string
 	dailyIndexes bool
+	useIndex     string
+	lastChk      time.Time
 	client       *elastic.Client
+}
+
+func (e *ElasticStorageModule) makeIndexIfNotExists() error {
+	index := e.index
+	if e.dailyIndexes {
+		index = time.Now().Format(fmt.Sprintf("%s-2006-1-2", e.index))
+	}
+
+	exists, err := e.client.IndexExists(index).Do(context.Background())
+	if err != nil {
+		return err
+	}
+
+	if !exists {
+		log.Printf("Creating new index %s", index)
+		e.client.CreateIndex(index).Body(mapping).Do(context.Background())
+	}
+
+	e.useIndex = index
+	return nil
 }
 
 // Initialize prepares the storage modules for use.
@@ -70,26 +93,26 @@ func (e *ElasticStorageModule) Initialize() error {
 	if err != nil {
 		return err
 	}
-	log.Printf("Elasticsearch returned with code %d and version %s", code, info.Version.Number)
+	log.Printf("Elasticsearch returned with code %d and version %s", code,
+		info.Version.Number)
 
-	exists, err := e.client.IndexExists(e.index).Do(context.Background())
+	err = e.makeIndexIfNotExists()
 	if err != nil {
 		return err
 	}
 
-	if !exists {
-		log.Printf("Creating new index %s", e.index)
-
-		e.client.CreateIndex(e.index).Do(context.Background())
-	}
-
-	return nil
+	e.lastChk = time.Now()
+	log.Printf("Using index %s", e.useIndex)
+	return err
 }
 
 // StorePaste stores a single paste in the storage backend.
 func (e *ElasticStorageModule) StorePaste(paste PasteFull) {
+	if e.dailyIndexes && time.Since(e.lastChk) > 12*time.Hour {
+		e.makeIndexIfNotExists()
+	}
 	_, err := e.client.Index().
-		Index(e.index).
+		Index(e.useIndex).
 		Type("paste").
 		Id(paste.Key).
 		BodyJson(paste).
--- main.go
+++ main.go
@@ -14,13 +14,15 @@ func main() {
 
 	elasticURL := flag.String("url", "http://localhost:9200", "elastic search api url")
 	index := flag.String("index", "pastebin", "pastebin")
+	dailyIndex := flag.Bool("daily", false, time.Now().Format("use daily indexes, e.g. pastebin-2006-1-2"))
 	APIServer := flag.String("server", "pastebin.com", "Pastebin scraping API url")
 	flag.Parse()
 
 	// make elastic storage module
 	e := &ElasticStorageModule{
-		index:      *index,
-		elasticURL: *elasticURL,
+		index:        *index,
+		dailyIndexes: *dailyIndex,
+		elasticURL:   *elasticURL,
 	}
 
 	// make and start fetch worker
